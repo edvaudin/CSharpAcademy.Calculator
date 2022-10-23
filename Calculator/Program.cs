9b0f67ff7fc8fe6a2b0fc421ffd7c280ffde6849
Edward Vaudin <edward@vaudin.net>
2022-10-23 21:50:21 +0100

    Fixed divide by zero exception.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -34,9 +34,16 @@ switch (Console.ReadLine())
         Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
         break;
     case "d":
+        // Ask the user to enter a non-zero divisor until they do so.
+        while (num2 == 0)
+        {
+            Console.WriteLine("Enter a non-zero divisor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+        }
         Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
         break;
 }
+
 // Wait for the user to respond before closing.
 Console.Write("Press any key to close the Calculator console app...");
 Console.ReadKey();
